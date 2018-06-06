199773d6c438e655969ab26992fd4118486546c6
Michał Suder <michalsuder@wp.pl>
2018-06-06 15:03:11 +0200

    Create repro

---
+++ MissingCollectionElements/API/Link.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissingCollectionElements.API
+{
+    public class Link
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(ContainerId))]
+        public Container Container { get; set; }
+
+        [ForeignKey(nameof(Container))]
+        public int ContainerId { get; set; }
+
+        [ForeignKey(nameof(ItemId))]
+        public Item Item { get; set; }
+
+        [ForeignKey(nameof(Item))]
+        public int ItemId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Link link &&
+                   ContainerId == link.ContainerId &&
+                   ItemId == link.ItemId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ContainerId, ItemId);
+        }
+    }
+}
---
+++ MissingCollectionElements/Tests/DataTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MissingCollectionElements.API;
+using Xunit;
+
+namespace MissingCollectionElements.Tests
+{
+    public class DataTest
+    {
+        private readonly DbContextOptions<DataContext> _options;
+
+        public DataTest()
+        {
+            var serviceProvider = new ServiceCollection()
+                                  .AddEntityFrameworkInMemoryDatabase()
+                                  .BuildServiceProvider();
+
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString()).UseInternalServiceProvider(serviceProvider);
+
+            _options = optionsBuilder.Options;
+
+            using (var context = CreateContext())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+
+        [Fact]
+        public void AddAndRemoveFromCollection()
+        {
+            // Given
+            var container = new Container();
+            var itemA = new Item();
+            var itemB = new Item();
+
+            container.ItemLinks = new List<Link>
+            {
+                new Link
+                {
+                    Item = itemA
+                }
+            };
+
+            using (var context = CreateContext())
+            {
+                context.AddRange(itemA, itemB, container);
+                context.SaveChanges();
+            }
+
+            // When
+            using (var context = CreateContext())
+            {
+                var dbContainer = context.Set<Container>()
+                                         .Include(c => c.ItemLinks)
+                                         .ThenInclude(l => l.Item)
+                                         .Single(c => c.Id == container.Id);
+
+                dbContainer.ItemLinks = new List<Link>
+                {
+                    new Link
+                    {
+                        ItemId = itemA.Id
+                    },
+                    new Link
+                    {
+                        ItemId = itemB.Id
+                    }
+                };
+
+                dbContainer.ItemLinks.Should().HaveCount(2);
+
+                context.SaveChanges();
+
+                // dbContainer.ItemLinks.Should().HaveCount(2); // <<-- Fail here because ItemLinks collection have just one element
+
+                var dbContainer2 = context.Set<Container>()
+                                          .Include(c => c.ItemLinks)
+                                          .ThenInclude(l => l.Item)
+                                          .Single(c => c.Id == container.Id);
+
+                // dbContainer2.ItemLinks.Should().HaveCount(2); // <<-- Fail here because ItemLinks collection have just one element
+            }
+
+            // Then
+            using (var context = CreateContext())
+            {
+                context.Set<Link>().Should().HaveCount(2);
+                var dbContainer = context.Set<Container>()
+                                         .Include(c => c.ItemLinks)
+                                         .ThenInclude(l => l.Item)
+                                         .Single(c => c.Id == container.Id);
+
+                dbContainer.ItemLinks.Should().HaveCount(2);
+            }
+
+        }
+
+        private DataContext CreateContext()
+        {
+            return new DataContext(_options);
+        }
+    }
+}
